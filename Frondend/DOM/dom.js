7e7086564aa1c071f22beb9e5f5a79523ee9d63c
Davidkuralan <thiruandshanker@gmail.com>
2023-02-03 14:31:48 +0530

    added border....changed title to green and bold.

---
+++ Frondend/DOM/dom.js
@@ -0,0 +1,5 @@
+let header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+let title = document.getElementsByClassName('title');
+title[0].style.color = 'green';
+title[0].style.fontWeight = '900'; 
