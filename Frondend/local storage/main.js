e198a6cac327c3f51e2ebfdea2e3569e8fceb006
Davidkuralan <thiruandshanker@gmail.com>
2023-02-05 15:21:27 +0530

    added phone number and local storage

--- Frondend/local storage/main.js
+++ Frondend/local storage/main.js
@@ -4,6 +4,7 @@
 const myForm = document.querySelector('#my-form');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
+const phoneInput = document.querySelector('#phone');
 const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 
@@ -13,7 +14,7 @@ myForm.addEventListener('submit', onSubmit);
 function onSubmit(e) {
   e.preventDefault();
   
-  if(nameInput.value === '' || emailInput.value === '') {
+  if(nameInput.value === '' || emailInput.value === '' || phoneInput.value === 0 ) {
     // alert('Please enter all fields');
     msg.classList.add('error');
     msg.innerHTML = 'Please enter all fields';
@@ -21,16 +22,31 @@ function onSubmit(e) {
     // Remove error after 3 seconds
     setTimeout(() => msg.remove(), 3000);
   } else {
-    let myObj = {
-        nameInput : `${nameInput.value}`,
-        emailInput :`${emailInput.value}`
-    }
-    let newObj = JSON.stringify(myObj);
+let myObj = {
+  Name : `${nameInput.value}`,
+  Email :`${emailInput.value}`,
+  Phone : `${phoneInput.value}`
+}
 
-    localStorage.setItem("Users" , newObj);
+let newObj = JSON.stringify(myObj);
+
+localStorage.setItem("User",newObj);
+
+    // Create new list item with user
+    const li = document.createElement('li');
+
+    // Add text node with input values
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value} : ${phoneInput.value}`));
+
+    // Add HTML
+    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+
+    // Append to ul
+    userList.appendChild(li);
 
     // Clear fields
     nameInput.value = '';
     emailInput.value = '';
+    phoneInput.value = '';
   }
 }
